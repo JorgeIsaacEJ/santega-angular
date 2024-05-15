5b359303e24ca0446923537fc03bbe1bc2d4b47a
Jorge <jorge_yugi@hotmail.es>
2024-05-15 13:59:41 -0600

    Cambio enviroment prod

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-  api: 'http://108.181.200.246/WebApicobranza',
+  //api: 'http://108.181.200.246/WebApicobranza',
+  api: 'http://localhost/WebApicobranza',
   version: 'api/v1',
   secretCrypto: 'ecinco#BF4.TT.T5RB0',
   nuvei: {
