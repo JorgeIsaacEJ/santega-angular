2e62c749670884e27790750fb3ec0050ab3a7d38
Jorge <jorge_yugi@hotmail.es>
2024-05-29 17:49:04 -0600

    Aviso de privacidad, estilos y focus

--- src/app/shared/components/layout/footer/footer.component.ts
+++ src/app/shared/components/layout/footer/footer.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { EventsService } from 'src/app/shared/services/events.service';
 
 @Component({
   selector: 'app-footer',
@@ -6,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
+
+  constructor(
+    private readonly eventsService: EventsService,
+    private readonly router: Router
+  ) { }
+
+  setSection( section: string ): void {
+
+    this.eventsService.setSectionSelected( section );
+    this.router.navigate(['/inicio'], {
+      queryParams: {
+        s: section
+      }
+    })
+  }
 
 }
